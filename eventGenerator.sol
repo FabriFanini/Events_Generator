0b60ec632f8f5234e76f03084cc6108974a760d1
Fabrizio Fanini <fabrifanini@gmail.com>
2025-02-15 13:13:55 -0300

    New Project

---
+++ eventGenerator.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ConquerEvents is ERC1155, Ownable {
+    //Errors
+
+    //Events
+    event newEvent(uint256 id, string name, uint256 totalSupply, uint256 price);
+
+    //Variables
+    uint256 eventId;
+
+    //Struct
+
+    struct Event {
+        uint256 id;
+        string name;
+        uint256 totalSupply;
+        uint256 availableTickets;
+        uint256 price;
+        bool isActive;
+    }
+
+    //Mappings
+    mapping (uint256 => Event) public _events;
+
+    //Constructor
+    constructor () ERC1155("https://api.example/metadata/{id}.json") Ownable(msg.sender) {
+        eventId = 0;
+    }
+
+    //Functions
+    function createEvent(string memory _name, uint256 _totalSupply, uint256 _price) external onlyOwner{
+        eventId = eventId + 1;
+
+        _events[eventId]= Event({
+            id:eventId,
+            name: _name,
+            totalSupply: _totalSupply,
+            availableTickets: _totalSupply,
+            price: _price,
+            isActive: true
+        });
+
+        emit newEvent(eventId, _name, _totalSupply, _price);
+    }
+
+    function cancelEvent(uint256 _eventId) external onlyOwner{
+        
+    }
+}
